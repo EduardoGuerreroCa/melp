2fc6f3030d673cd36e6b3260d89093adebacfdf6
EduardoGuerreroCa <18030547@itcelaya.edu.mx>
2024-05-08 00:03:26 -0600

    Se añadio morgan al index.js para ver las peticiones que se hacen en el backend

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import {PORT} from './config.js';
 import restaurantRoutes from './routes/restaurants.routes.js'
+import morgan from 'morgan'
 
 const app = express()
 
+app.use(morgan('dev'))
 // Para que pueda entender los datos json
 app.use(express.json());
 app.use(restaurantRoutes);
