685c7dfed5d548d6b159ef9161be97e5e03c49f8
EduardoGuerreroCa <18030547@itcelaya.edu.mx>
2024-05-07 20:39:15 -0600

    Creacion de rutas

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import {PORT} from './config.js';
+import restaurantRoutes from './routes/restaurants.routes.js'
 
 const app = express()
+
+app.use(restaurantRoutes);
 
 app.listen(PORT)
 console.log('Server on port', PORT)
---
+++ src/routes/restaurants.routes.js
@@ -0,0 +1,26 @@
+import {Router} from 'express'
+
+const router = Router();
+
+router.get('/restaurants', (req, res) => {
+    res.send('Getting restaurants')
+})
+
+router.get('/restaurants/:id', (req, res) => {
+    const {id} = req.params
+    res.send('Getting restaurant: ' + id)
+})
+
+router.post('/restaurants', (req, res) => {
+    res.send('Creating restaurants')
+})
+
+router.delete('/restaurants/:id', (req, res) => {
+    res.send('Deleting restaurants')
+})
+
+router.put('/restaurants/:id', (req, res) => {
+    res.send('Getting restaurants')
+})
+
+export default router;
